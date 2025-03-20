50320e3f0d33a4c61c355970cc62aed33e94dcd0
TrungK2Tan <115651324+TrungK2Tan@users.noreply.github.com>
2025-03-20 13:00:55 +0700

    portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.querySelector(".btn");
+    btn.addEventListener("click", function () {
+        alert("Cảm ơn bạn đã quan tâm!");
+    });
+});
